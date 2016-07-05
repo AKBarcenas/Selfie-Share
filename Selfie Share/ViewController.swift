7490beb90b8c45bb33cd48e2b54b58738d1e5451
AKBarcenas <akbarcen@ucsd.edu>
2016-07-05 03:13:03 -0700

    Allow the user to create and join sessions.

--- Selfie Share/ViewController.swift
+++ Selfie Share/ViewController.swift
@@ -7,18 +7,32 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate, MCSessionDelegate, MCBrowserViewControllerDelegate {
     // The collection view displaying the images.
     @IBOutlet weak var collectionView: UICollectionView!
     // All of the images used by the app.
     var images = [UIImage]()
+    
+    // Identifies the user.
+    var peerID: MCPeerID!
+    // Handles multipeer conectivity.
+    var mcSession: MCSession!
+    // Handles session creation and session advertisement.
+    var mcAdvertiserAssistant: MCAdvertiserAssistant!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Selfie Share"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Camera, target: self, action: #selector(importPicture))
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(showConnectionPrompt))
+        
+        peerID = MCPeerID(displayName: UIDevice.currentDevice().name)
+        mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .Required)
+        mcSession.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -106,6 +120,46 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    /*
+     * Function Name: showConnectionPrompt
+     * Parameters: None
+     * Purpose: This method prompts the user what type of connection they want to make.
+     * Return Value: None
+     */
+    
+    func showConnectionPrompt() {
+        let ac = UIAlertController(title: "Connect to others", message: nil, preferredStyle: .ActionSheet)
+        ac.addAction(UIAlertAction(title: "Host a session", style: .Default, handler: startHosting))
+        ac.addAction(UIAlertAction(title: "Join a session", style: .Default, handler: joinSession))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        presentViewController(ac, animated: true, completion: nil)
+    }
+    
+    /*
+     * Function Name: startHosting
+     * Parameters: action - the action associated with calling this method.
+     * Purpose: This method creates a session that the user is the host of.
+     * Return Value: None
+     */
+    
+    func startHosting(action: UIAlertAction!) {
+        mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "hws-project25", discoveryInfo: nil, session: mcSession)
+        mcAdvertiserAssistant.start()
+    }
+    
+    /*
+     * Function Name: joinSession
+     * Parameters: action - the action associated with calling this method.
+     * Purpose: This method allows the user to join a session made by another user.
+     * Return Value: None
+     */
+    
+    func joinSession(action: UIAlertAction!) {
+        let mcBrowser = MCBrowserViewController(serviceType: "hws-project25", session: mcSession)
+        mcBrowser.delegate = self
+        presentViewController(mcBrowser, animated: true, completion: nil)
     }
 
 }
